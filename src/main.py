9b0683011fb585a84a215822828d7f35becdc934
chknofthewoods <cotwgames@gmail.com>
2025-06-11 16:15:46 -0600

    updated site generator for github pages

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import sys
 from common import *
 
 def copy_directory(src, dst):
@@ -17,7 +18,7 @@ def copy_directory(src, dst):
             else:
                 shutil.copy(src_path, dst_path)
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     if not os.path.exists(from_path) or not os.path.isfile(from_path):
         raise Exception(f"Source file {from_path} does not exist.")
     if not os.path.exists(template_path) or not os.path.isfile(template_path):
@@ -35,13 +36,13 @@ def generate_page(from_path, template_path, dest_path):
         template = f.read()
     
     page_content = markdown_to_htmlnode(content).to_html()
-    full_page = template.replace("{{ Content }}", page_content)
-    full_page = full_page.replace("{{ Title }}", extract_title(content))
+    full_page = template.replace("{{ Content }}", page_content).replace("{{ Title }}", extract_title(content))
+    full_page = full_page.replace('href="/', f'href="{basepath}').replace('src="/', f'src="{basepath}')
     
     with open(dest_path, 'w') as f:
         f.write(full_page)
 
-def generate_page_recursive(from_dir_path, template_path, dest_dir_path):
+def generate_page_recursive(from_dir_path, template_path, dest_dir_path, basepath):
     if not os.path.exists(from_dir_path) or not os.path.isdir(from_dir_path):
         raise Exception(f"Source directory {from_dir_path} does not exist.")
     if not os.path.exists(template_path) or not os.path.isfile(template_path):
@@ -51,22 +52,27 @@ def generate_page_recursive(from_dir_path, template_path, dest_dir_path):
         file_path = os.path.join(from_dir_path, file)
         if os.path.isdir(file_path):
             new_dest_dir = os.path.join(dest_dir_path, file)
-            generate_page_recursive(file_path, template_path, new_dest_dir)
+            generate_page_recursive(file_path, template_path, new_dest_dir, basepath)
         elif file.endswith(".md"):
             dest_file_path = os.path.join(dest_dir_path, file.replace(".md", ".html"))
-            generate_page(file_path, template_path, dest_file_path)
+            generate_page(file_path, template_path, dest_file_path, basepath)
 
-def generate_static_site(from_path, template_path, dest_path):
+def generate_static_site(from_path, template_path, dest_path, basepath):
     static = "/home/deck/Documents/workspace/github/static-site-generator/static"
-    public = "/home/deck/Documents/workspace/github/static-site-generator/public"
+    public = "/home/deck/Documents/workspace/github/static-site-generator/docs"
     copy_directory(static, public)
-    generate_page_recursive(from_path, template_path, dest_path)
+    generate_page_recursive(from_path, template_path, dest_path, basepath)
 
 def main():
+    basepath = sys.argv[1:]
+    if not basepath:
+        basepath = "/"
+
     generate_static_site(
         "/home/deck/Documents/workspace/github/static-site-generator/content",
         "/home/deck/Documents/workspace/github/static-site-generator/template.html",
-        "/home/deck/Documents/workspace/github/static-site-generator/public"
+        "/home/deck/Documents/workspace/github/static-site-generator/docs",
+        basepath
     )
 
 main()
